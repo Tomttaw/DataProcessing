b651877878eb612ffb517d2e9360026a1b0431db
Tomtttaw <tom-s@live.nl>
2016-04-22 18:20:13 +0200

    week2

---
+++ week2/main2.js
@@ -0,0 +1,17 @@
+/* use this to test out your function */
+window.onload = function() {
+  changeColor("esp", "green");
+  changeColor("fr", "yellow");
+  changeColor("gre", "purple");
+  changeColor("mdv", "pink");
+};
+
+/* changeColor takes a path ID and a color (hex value)
+   and changes that path's fill color */
+function changeColor(id, color) {
+    // get country by id
+    var country = document.getElementById(id);
+    
+    // change color 
+    country.style.fill=color;    
+}
