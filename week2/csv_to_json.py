e87577e2d2ebf13bb664940454008474127d2ec3
Tomtttaw <tom-s@live.nl>
2016-05-05 16:34:50 +0200

    Chart without axes

--- week2/csv_to_json.py
+++ week2/csv_to_json.py
@@ -8,14 +8,14 @@ Created on Wed Apr 20 12:11:53 2016
 import json
 import csv
 
-csvfile = open('europa_gini.csv', 'r')
-jsonfile = open('income_equality.json', 'w')
+csvfile = open('regenval.csv', 'r')
+jsonfile = open('regenval.json', 'w')
 
-fieldnames = ("Country_code", "gini_index")
+fieldnames = ("Date", "rain")
 reader = csv.DictReader(csvfile, fieldnames)
 for row in reader:
     json.dump(row, jsonfile)
-    jsonfile.write("\n")
+    jsonfile.write(",\n")
 
 csvfile.close()
 jsonfile.close()    
